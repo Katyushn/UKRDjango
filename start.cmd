519a8e499a36d0da01b59cca15cc4758b513d318
Volodymyr <31333464+Katyushn@users.noreply.github.com>
2023-03-11 12:23:18 +0200

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,11 +1,9 @@
-IF EXIST "C:\Program Files\Python311\python.exe" (
+IF EXIST "venv\Scripts\activate.ps1" (
     venv\Scripts\activate
     cd app
     start chrome http://127.0.0.1/admin/
     python manage.py runserver 80
 ) ELSE (
-    python-3.11.1-amd64.exe InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
-    gettext0.21-iconv1.16-static-64.exe
     python -m venv venv
     venv\Scripts\activate
     pip install -r requirements.txt
